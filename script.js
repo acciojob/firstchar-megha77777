fd379d639e3ecc82ee37e98cb5ddfa839482d05b
megha77777 <141833704+megha77777@users.noreply.github.com>
2023-12-22 22:33:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstChar(text) {
   // your code here
+	text = text.trim();
+	if(text.length>0){
+		return text[0];
+	}
+	else{
+		return "";
+	}
+	
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
